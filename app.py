78e33e29f2a8c770961c32718a4887f25f27fdaf
peter-ntc <73801646+peter-ntc@users.noreply.github.com>
2025-06-19 21:54:17 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -49,6 +49,19 @@ def safe_load_df(filename, scenario_label):
 def render_option(option_num):
     if option_num == "1":
         render_forecasting_modeling()
+    elif option_num == "2" or option_num == "3" or option_num == "4":
+        option_names = [
+            "Forecasting & Modeling",
+            "Optimizer",
+            "Fund & Deal Pipeline",
+            "Smart Benchmarks",
+            "Secondaries Marketplace",
+            "Market Research"
+        ]
+        st.title(option_names[int(option_num)-1])
+        st.subheader("🚧 Under Construction 🚧")
+        st.markdown("<br>", unsafe_allow_html=True)
+        st.button("🔙 Return to Home", on_click=go_home, use_container_width=True)
     else:
         option_names = [
             "Forecasting & Modeling",
@@ -81,6 +94,334 @@ def render_forecasting_modeling():
             df = safe_load_df(file, label)
             if df is not None:
                 plot_chart(df, label)
+            if label == "Consensus Economic Outlook":
+                st.markdown("### Sector Summary Table")
+                st.markdown("""<table border="1" class="dataframe">
+  <thead>
+    <tr style="text-align: right;">
+      <th>Sector</th>
+      <th>Entry Cap Rate</th>
+      <th>Exit Cap Rate Year 3</th>
+      <th>Exit Cap Rate Year 6</th>
+      <th>Rent Growth Year 1</th>
+      <th>Rent Growth Year 2</th>
+      <th>Rent Growth Year 3</th>
+      <th>Rent Growth Year 4</th>
+      <th>Rent Growth Year 5</th>
+      <th>Rent Growth Year 6</th>
+      <th>3 Yr Unlevered Property Returns</th>
+      <th>6 Yr Unlevered Property Returns</th>
+      <th>3 Yr Levered Fund Net Returns</th>
+      <th>6 Yr Levered Fund Net Returns</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td>Core - Res</td>
+      <td>4.60%</td>
+      <td>2.97%</td>
+      <td>4.76%</td>
+      <td>-1.20%</td>
+      <td>1.00%</td>
+      <td>2.40%</td>
+      <td>4.00%</td>
+      <td>5.00%</td>
+      <td>2.50%</td>
+      <td>19.86%</td>
+      <td>5.60%</td>
+      <td>19.36%</td>
+      <td>5.05%</td>
+    </tr>
+    <tr>
+      <td>Core - Ind</td>
+      <td>3.70%</td>
+      <td>3.48%</td>
+      <td>4.64%</td>
+      <td>6.10%</td>
+      <td>5.00%</td>
+      <td>4.30%</td>
+      <td>4.00%</td>
+      <td>3.50%</td>
+      <td>3.00%</td>
+      <td>10.60%</td>
+      <td>4.16%</td>
+      <td>10.35%</td>
+      <td>3.73%</td>
+    </tr>
+    <tr>
+      <td>Core - Off</td>
+      <td>5.60%</td>
+      <td>9.92%</td>
+      <td>7.79%</td>
+      <td>-1.80%</td>
+      <td>-1.10%</td>
+      <td>-0.80%</td>
+      <td>0.50%</td>
+      <td>1.00%</td>
+      <td>2.00%</td>
+      <td>-13.63%</td>
+      <td>-0.97%</td>
+      <td>-14.63%</td>
+      <td>-3.96%</td>
+    </tr>
+    <tr>
+      <td>Core - Retail</td>
+      <td>5.50%</td>
+      <td>6.16%</td>
+      <td>5.54%</td>
+      <td>1.00%</td>
+      <td>2.00%</td>
+      <td>2.00%</td>
+      <td>2.00%</td>
+      <td>2.00%</td>
+      <td>2.50%</td>
+      <td>2.60%</td>
+      <td>6.32%</td>
+      <td>1.85%</td>
+      <td>5.63%</td>
+    </tr>
+    <tr>
+      <td>Core-Plus - Res</td>
+      <td>4.80%</td>
+      <td>2.97%</td>
+      <td>4.76%</td>
+      <td>-1.60%</td>
+      <td>0.60%</td>
+      <td>2.30%</td>
+      <td>4.00%</td>
+      <td>5.00%</td>
+      <td>2.50%</td>
+      <td>21.26%</td>
+      <td>6.25%</td>
+      <td>21.01%</td>
+      <td>6.38%</td>
+    </tr>
+    <tr>
+      <td>Core-Plus - Ind</td>
+      <td>4.10%</td>
+      <td>4.06%</td>
+      <td>5.22%</td>
+      <td>6.60%</td>
+      <td>5.90%</td>
+      <td>5.00%</td>
+      <td>3.50%</td>
+      <td>3.00%</td>
+      <td>2.50%</td>
+      <td>9.92%</td>
+      <td>4.46%</td>
+      <td>9.42%</td>
+      <td>3.80%</td>
+    </tr>
+    <tr>
+      <td>Core-Plus - Off</td>
+      <td>5.40%</td>
+      <td>9.92%</td>
+      <td>7.79%</td>
+      <td>-1.40%</td>
+      <td>-0.70%</td>
+      <td>-0.30%</td>
+      <td>0.50%</td>
+      <td>1.00%</td>
+      <td>2.00%</td>
+      <td>-14.35%</td>
+      <td>-1.40%</td>
+      <td>-15.60%</td>
+      <td>-5.92%</td>
+    </tr>
+    <tr>
+      <td>Core-Plus - Ret</td>
+      <td>5.90%</td>
+      <td>6.16%</td>
+      <td>5.54%</td>
+      <td>1.00%</td>
+      <td>2.00%</td>
+      <td>2.00%</td>
+      <td>2.00%</td>
+      <td>2.00%</td>
+      <td>2.50%</td>
+      <td>5.15%</td>
+      <td>7.72%</td>
+      <td>4.15%</td>
+      <td>8.24%</td>
+    </tr>
+    <tr>
+      <td>Data Center</td>
+      <td>5.40%</td>
+      <td>3.09%</td>
+      <td>5.56%</td>
+      <td>5.90%</td>
+      <td>5.90%</td>
+      <td>5.10%</td>
+      <td>5.00%</td>
+      <td>4.00%</td>
+      <td>3.00%</td>
+      <td>31.13%</td>
+      <td>9.00%</td>
+      <td>30.88%</td>
+      <td>10.36%</td>
+    </tr>
+    <tr>
+      <td>Single-Family Rental</td>
+      <td>4.50%</td>
+      <td>3.47%</td>
+      <td>4.63%</td>
+      <td>4.80%</td>
+      <td>4.80%</td>
+      <td>4.90%</td>
+      <td>4.00%</td>
+      <td>4.00%</td>
+      <td>3.00%</td>
+      <td>18.05%</td>
+      <td>7.90%</td>
+      <td>17.55%</td>
+      <td>8.00%</td>
+    </tr>
+    <tr>
+      <td>Cold Storage</td>
+      <td>3.80%</td>
+      <td>4.22%</td>
+      <td>6.63%</td>
+      <td>10.00%</td>
+      <td>8.00%</td>
+      <td>5.00%</td>
+      <td>4.00%</td>
+      <td>3.00%</td>
+      <td>2.00%</td>
+      <td>7.54%</td>
+      <td>0.35%</td>
+      <td>6.79%</td>
+      <td>-2.20%</td>
+    </tr>
+    <tr>
+      <td>Senior Housing</td>
+      <td>5.20%</td>
+      <td>3.90%</td>
+      <td>5.19%</td>
+      <td>8.50%</td>
+      <td>6.10%</td>
+      <td>5.10%</td>
+      <td>4.00%</td>
+      <td>4.00%</td>
+      <td>3.00%</td>
+      <td>21.26%</td>
+      <td>9.45%</td>
+      <td>20.51%</td>
+      <td>10.69%</td>
+    </tr>
+    <tr>
+      <td>Student Housing</td>
+      <td>5.75%</td>
+      <td>5.06%</td>
+      <td>5.62%</td>
+      <td>3.00%</td>
+      <td>3.00%</td>
+      <td>3.00%</td>
+      <td>2.50%</td>
+      <td>2.50%</td>
+      <td>2.00%</td>
+      <td>12.57%</td>
+      <td>8.30%</td>
+      <td>11.82%</td>
+      <td>8.64%</td>
+    </tr>
+    <tr>
+      <td>Affordable Housing</td>
+      <td>5.60%</td>
+      <td>4.30%</td>
+      <td>4.30%</td>
+      <td>3.00%</td>
+      <td>3.50%</td>
+      <td>3.00%</td>
+      <td>3.00%</td>
+      <td>3.00%</td>
+      <td>3.00%</td>
+      <td>17.60%</td>
+      <td>12.57%</td>
+      <td>16.85%</td>
+      <td>14.75%</td>
+    </tr>
+    <tr>
+      <td>Manufactured Home Park</td>
+      <td>4.00%</td>
+      <td>4.72%</td>
+      <td>4.72%</td>
+      <td>5.00%</td>
+      <td>5.00%</td>
+      <td>4.00%</td>
+      <td>3.00%</td>
+      <td>3.00%</td>
+      <td>3.00%</td>
+      <td>2.79%</td>
+      <td>4.81%</td>
+      <td>2.04%</td>
+      <td>3.71%</td>
+    </tr>
+    <tr>
+      <td>Medical Office</td>
+      <td>5.60%</td>
+      <td>5.17%</td>
+      <td>5.52%</td>
+      <td>2.50%</td>
+      <td>2.50%</td>
+      <td>2.50%</td>
+      <td>2.50%</td>
+      <td>2.30%</td>
+      <td>2.20%</td>
+      <td>10.12%</td>
+      <td>7.64%</td>
+      <td>9.37%</td>
+      <td>7.70%</td>
+    </tr>
+    <tr>
+      <td>Life Science</td>
+      <td>5.30%</td>
+      <td>5.81%</td>
+      <td>5.81%</td>
+      <td>3.90%</td>
+      <td>2.80%</td>
+      <td>2.40%</td>
+      <td>2.50%</td>
+      <td>2.50%</td>
+      <td>2.50%</td>
+      <td>4.77%</td>
+      <td>6.08%</td>
+      <td>3.77%</td>
+      <td>5.12%</td>
+    </tr>
+    <tr>
+      <td>Self-Storage</td>
+      <td>4.40%</td>
+      <td>5.16%</td>
+      <td>5.71%</td>
+      <td>-1.50%</td>
+      <td>2.30%</td>
+      <td>2.70%</td>
+      <td>2.50%</td>
+      <td>2.00%</td>
+      <td>2.00%</td>
+      <td>0.16%</td>
+      <td>1.86%</td>
+      <td>-0.59%</td>
+      <td>-0.51%</td>
+    </tr>
+    <tr>
+      <td>IOS</td>
+      <td>3.70%</td>
+      <td>3.76%</td>
+      <td>5.91%</td>
+      <td>7.00%</td>
+      <td>6.00%</td>
+      <td>5.00%</td>
+      <td>4.00%</td>
+      <td>3.00%</td>
+      <td>2.00%</td>
+      <td>9.09%</td>
+      <td>1.12%</td>
+      <td>8.59%</td>
+      <td>-1.01%</td>
+    </tr>
+  </tbody>
+</table>""", unsafe_allow_html=True)
 
     # Comparison chart
     st.markdown("###")
